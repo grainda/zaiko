3facddadd72219a9b401795204babd45d43f38ee
mrms <straight0grain@gmail.com>
2025-08-13 18:08:31 +0900

    modalにアイコン、場所、商品名、在庫の入力スペース追加

--- screens/Store.tsx
+++ screens/Store.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,30 +7,38 @@ import {
   Modal,
   Image,
   FlatList,
+  TextInput,
 } from "react-native";
 const icons = [
   { id: "1", source: require("../assets/png/001-soap.png") },
   { id: "2", source: require("../assets/png/002-vegetables.png") },
   { id: "3", source: require("../assets/png/003-meat.png") },
-  { id: "4", source: require("../assets/png/004-cosmetics.png") },
-  { id: "5", source: require("../assets/png/005-pet.png") },
-  { id: "6", source: require("../assets/png/006-sauces.png") },
-  { id: "7", source: require("../assets/png/007-paper.png") },
-  { id: "8", source: require("../assets/png/008-toothpaste.png") },
+  { id: "4", source: require("../assets/png/004-pet.png") },
+  { id: "5", source: require("../assets/png/005-paper.png") },
+  { id: "6", source: require("../assets/png/006-toothpaste.png") },
+  { id: "7", source: require("../assets/png/007-cosmetics.png") },
+  { id: "8", source: require("../assets/png/008-cleaner.png") },
+  { id: "9", source: require("../assets/png/009-snacks.png") },
 ];
 
 const Store: React.FC = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedIcon, setSelectedIcon] = useState(null);
+  const [itemName, setItemName] = useState("");
+  const [itemPlace, setItemPlace] = useState("");
+  const [itemNumber, setItemNumber] = useState(0);
+  const addNumber = () => {
+    setItemNumber(itemNumber + 1);
+  };
 
   const renderIcon = ({ item }) => (
     <TouchableOpacity onPress={() => setSelectedIcon(item.source)}>
       <Image
         source={item.source}
-        // style={[
-        //   styles.icon,
-        //   selectedIcon === item.source && styles.selectedIcon, // 選択時に枠線を追加
-        // ]}
+        style={[
+          styles.icon,
+          selectedIcon === item.source && styles.selectedIcon,
+        ]}
       />
     </TouchableOpacity>
   );
@@ -67,23 +75,40 @@ const Store: React.FC = () => {
           </View>
           <View style={[styles.modalContainer]}>
             <Text style={[styles.modalText]}>アイコンを選択</Text>
-            {selectedIcon && (
-              <View>
-                <Text style={styles.modalText}>選択されたアイコン:</Text>
-                <Image source={selectedIcon} style={styles.icon} />
-              </View>
-            )}
+
             <FlatList
               data={icons}
               renderItem={renderIcon}
               keyExtractor={(item) => item.id}
               numColumns={3}
-              // contentContainerStyle={styles.iconList}
+              contentContainerStyle={[styles.iconList]}
             />
-            <Image
-              source={require("../assets/png/001-soap.png")}
-              style={[styles.icon]}
-            ></Image>
+            <View style={[styles.modalItem]}>
+              <Text style={[styles.modalText]}>置き場所</Text>
+              <TextInput
+                style={styles.input}
+                value={itemPlace}
+                onChangeText={setItemPlace}
+                placeholder="必要な場合は置き場所を入力"
+                placeholderTextColor="#999"
+              />
+            </View>
+            <View style={[styles.modalItem]}>
+              <Text style={[styles.modalText]}>商品名</Text>
+              <TextInput
+                style={styles.input}
+                value={itemName}
+                onChangeText={setItemName}
+                placeholder="商品名を入力"
+                placeholderTextColor="#999"
+              />
+            </View>
+            <View style={styles.modalItem}>
+              <Text style={[styles.modalText]}>在庫</Text>
+              <TouchableOpacity onPress={() => addNumber()}>+</TouchableOpacity>
+              <TouchableOpacity onPress={() => subNimber()}>-</TouchableOpacity>
+              <Text>{itemNumber}</Text>
+            </View>
           </View>
           <View></View>
         </View>
@@ -136,14 +161,25 @@ const styles = StyleSheet.create({
   },
   modalText: {
     color: "#1DA1F2",
+    fontSize: 20,
   },
   modalContainer: {
     padding: 20,
+  },
+  modalItem: {
+    paddingTop: 40,
+  },
+  input: {
+    paddingTop: 20,
   },
   icon: {
     height: 80,
     width: 80,
   },
+  selectedIcon: {
+    backgroundColor: "#1DA1F2",
+  },
+  iconList: {},
 });
 
 export default Store;
