892154c202d433cb508137760aec5b1860df589d
mrms <straight0grain@gmail.com>
2025-09-03 16:14:57 +0900

    itemsを持ち運びたい

--- screens/List.tsx
+++ screens/List.tsx
@@ -1,12 +1,25 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList  } from "react-native";
 
 const List: React.FC = () => {
+  // const listItems =async () => {
+  //   const storedItems = await AsyncStorage.getItem("items");
+  //   const parsedItems = storedItems ? JSON.parse(storedItems) : [];
+  //   const listItem = parsedItems.filter(listIn => itemNumber =< 1)
+  // };
+  const renderList
   return (
     <View>
-      <View></View>
+         <FlatList
+              data={items}
+              renderItem={renderList}
+              keyExtractor={(item) => item.id}
+              numColumns={2}
+            />
+      <View>
       <Text>・買い物リスト</Text>
-      <View></View>
+      </View>
     </View>
   );
 };
